a4d3c5617322a0285c6d98e40f5889c6cd90c737
Arpit Tiwari <109851906+arpitttiwari@users.noreply.github.com>
2022-09-18 16:32:55 +0530

    Update EMIcalc.py

--- Day-4-Newzealand/EMIcalc.py
+++ Day-4-Newzealand/EMIcalc.py
@@ -1,1 +1,11 @@
+#LOAN EMI CALCULATOR
+def EMI(p,r,n):
+    return (p*r*((1+r)**n))/((1+r)**n-1)
+    
+p=float(input("Enter the princicpal amount:"))
+R=float(input("Enter the rate of interest in % per annum:"))
+r=R/12/100
+N=float(input("Enter the no of years in whch you want to pay back:"))
+n=N*12
 
+print("Your monthly EMI is Rs. {}".format(EMI(p,r,n)))
