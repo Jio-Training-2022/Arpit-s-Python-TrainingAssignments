3dc82ca4dbde4c99fd52f17b6f982c2812bb70fb
Arpit Tiwari <109851906+arpitttiwari@users.noreply.github.com>
2022-09-18 15:08:49 +0530

    Add files via upload

---
+++ Day-2-Vietnam/SIcalc.py
@@ -0,0 +1,8 @@
+
+
+A=int(input("Enter the Principal (in Rs):"))
+R=int(input("Enter the rate % (per annum):"))
+T=int(input("Enter the time (in years):"))
+
+SI=(A*R*T)/100
+print('The simple interest is Rs {}.'.format(SI))
