5c7eb8659fadf41cf1052037c40b082aebd35e8b
Arpit Tiwari <109851906+arpitttiwari@users.noreply.github.com>
2022-09-21 20:41:27 +0530

    Update secondlargest.py

--- Day-4-Newzealand/secondlargest.py
+++ Day-4-Newzealand/secondlargest.py
@@ -1,13 +1,13 @@
 # THROUGH single loop LOOPS
 n=int(input("Enter the no of elements in an array:"))
-arr=[]
+A=[]
 for i in range(0,n):
     ele=int(input())
-    arr.append(ele)
+    A.append(ele)
 
 largest=0
 secondlargest=0
-for i in arr: 
+for i in A: 
     if i > largest:
         secondlargest = largest
         largest=i
@@ -20,17 +20,17 @@ print("Second largest element is: ",secondlargest)
 
 """ # USING TWO LOOPS
 n=int(input("Enter the no of elements in an array:"))
-arr=[]
+A=[]
 for i in range(0,n):
     ele=int(input())
-    arr.append(ele)
+    A.append(ele)
 
 largest=0
-for i in arr:
+for i in A:
      if largest<i:
          largest=i
 secondlargest=0        
-for j in arr:
+for j in A:
      if (j<largest) & (j>secondlargest):
          secondlargest=j
      else:
